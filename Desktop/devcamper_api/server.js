5954a1b59cd5fa03e1812e52ecc4e4f5a1f260f2
Tofunmi <olaniyanolaoluwa3@gmail.com>
2021-07-10 00:06:56 +0100

    server.js

--- Desktop/devcamper_api/server.js
+++ Desktop/devcamper_api/server.js
@@ -1,12 +1,13 @@
 const express= require('express');
 const dotenv = require('dotenv');
+const morgan = require ('morgan');
 
 dotenv.config({ path: './DEVCAMPER_API/.env' });
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, msg: 'show all bootcamps'})
 });
 const PORT = process.env.PORT || 5000
 
